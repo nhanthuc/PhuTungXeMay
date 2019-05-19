971340c35f2915358101a0fd85e878f64d9b1ed0
nhanthuc <33113863+nhanthuc@users.noreply.github.com>
2019-05-19 14:13:27 +0700

    get2

---
+++ src/model/Detail_Order.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Detail_Order {
+	private long id;
+	private long id_Oder;
+	private long product_id;
+	private long quantity;
+	private long magio_hang;
+	public Detail_Order() {
+		
+	}
+	public Detail_Order(long id, long oder_id, long product_id, long quantity, long magio_hang) {
+		super();
+		this.id = id;
+		this.id_Oder = oder_id;
+		this.product_id = product_id;
+		this.quantity = quantity;
+		this.magio_hang = magio_hang;
+	}
+	public long getId_Oder() {
+		return id_Oder;
+	}
+	public void setId_Oder(long id_Oder) {
+		this.id_Oder = id_Oder;
+	}
+	public long getMagio_hang() {
+		return magio_hang;
+	}
+	public void setMagio_hang(long magio_hang) {
+		this.magio_hang = magio_hang;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getProduct_id() {
+		return product_id;
+	}
+	public void setProduct_id(long product_id) {
+		this.product_id = product_id;
+	}
+	public long getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(long quantity) {
+		this.quantity = quantity;
+	}
+}
---
+++ src/model/Orders.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.sql.Date;
+
+public class Orders {
+	private long id_oder;
+	private long custumer_id;
+	private double total_money;
+	private Date date;
+	private String status;
+	public Orders() {
+	}
+	
+	public Orders(long id_oder, long custumer_id, double total_money, Date date, String status) {
+		super();
+		this.id_oder = id_oder;
+		this.custumer_id = custumer_id;
+		this.total_money = total_money;
+		this.date = date;
+		this.status = status;
+	}
+
+	public long getId_oder() {
+		return id_oder;
+	}
+	public void setId_oder(long id_oder) {
+		this.id_oder = id_oder;
+	}
+	public long getCustumer_id() {
+		return custumer_id;
+	}
+	public void setCustumer_id(long custumer_id) {
+		this.custumer_id = custumer_id;
+	}
+	public double getTotal_money() {
+		return total_money;
+	}
+	public void setTotal_money(double total_money) {
+		this.total_money = total_money;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
