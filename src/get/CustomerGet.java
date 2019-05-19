57772415ff27e6aee3cb170e0056849ff4018a3c
nhanthuc <33113863+nhanthuc@users.noreply.github.com>
2019-05-19 15:20:04 +0700

    get3

--- src/get/CustomerGet.java
+++ src/get/CustomerGet.java
@@ -1,5 +1,133 @@
 package get;
 
-public class CustomerGet {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import model.ConnectToDatabase;
+import model.Customers;
+
+public class CustomerGet {
+	 public ArrayList<Customers> getListUser() throws SQLException {
+	        Connection connection = ConnectToDatabase.getConnecttion();
+	        String sql = "SELECT * FROM Custumers";
+	        PreparedStatement ps = connection.prepareCall(sql);
+	        ResultSet rs = ps.executeQuery();
+	        ArrayList<Customers> list = new ArrayList<>();
+	        while (rs.next()) {
+	        	Customers customer = new Customers();
+	        	customer.setId(rs.getLong("Id"));
+	        	customer.setName(rs.getString("Name"));
+	        	customer.setBirth(rs.getDate("BirthDay"));
+	        	customer.setProvince(rs.getString("province"));
+	        	customer.setAddress(rs.getString("Address"));
+	        	customer.setTel(rs.getString("Tel"));
+	        	customer.setEmail(rs.getString("mail"));
+	        	customer.setPassword(rs.getString("Password"));
+	        	
+	           
+	        
+	           
+	           list.add(customer);
+	        }
+	        return list;
+	    }
+	   
+	    
+	    public boolean checkEmail(String name) throws SQLException{
+	    Connection connection = ConnectToDatabase.getConnecttion();
+	    String sql = "SELECT * FROM Custumers WHERE Name = '" + name + "'";
+	    PreparedStatement ps;
+	    try {
+	        ps = connection.prepareCall(sql);
+	        ResultSet rs = ps.executeQuery();
+	        while(rs.next()){
+	            connection.close();
+	            return true;
+	        }
+	    } catch (SQLException ex) {
+	        Logger.getLogger(UserGet.class.getName()).log(Level.SEVERE,null, ex);
+	    }
+	    return false;
+	    }
+	    public boolean insertUser(Customers u) throws SQLException {
+	        Connection connection = ConnectToDatabase.getConnecttion();
+	        String sql = "INSERT INTO Custumers VALUES(?,?,?,?,?)";
+	        try {
+	            PreparedStatement ps = connection.prepareCall(sql);
+	            ps.setLong(1, u.getId());
+	            ps.setString(2,u.getName());
+	            ps.setString(3, u.getEmail());
+	            ps.setString(4, u.getPassword());
+	            ps.setString(4, u.getTel());
+	            ps.executeUpdate();
+	            return true;
+	        } catch (SQLException ex) {
+	            Logger.getLogger(UserGet.class.getName()).log(Level.SEVERE, null, ex);
+	        }
+	        return false;
+	    }
+	   
+	     public Customers login(String name, String password) throws SQLException {
+	        Connection con = ConnectToDatabase.getConnecttion();
+	        String sql = "select * from Custumers where Name='" + name + "' and Password='" + password + "'";
+	        PreparedStatement ps;
+	        try {
+	            ps = (PreparedStatement) con.prepareStatement(sql);
+	            ResultSet rs = ps.executeQuery();
+	            if (rs.next()) {
+	                Customers u = new Customers();
+	                u.setId(rs.getLong("Id"));
+	                u.setName(rs.getString("Name"));
+	 	           	u.setPassword(rs.getString("Password"));
+	                con.close();
+	                return u;
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return null;
+	    }
+	     
+	     public Customers getCustomers(long userID) throws SQLException {
+	     Connection connection = ConnectToDatabase.getConnecttion();
+	     String sql = "SELECT * FROM Customers WHERE Id = '" + userID + "'";
+	     PreparedStatement ps = connection.prepareCall(sql);
+	     ResultSet rs = ps.executeQuery();
+	     Customers user = new Customers();
+	     while (rs.next()) {
+	         
+	          	user.setId(rs.getLong("Id"));
+	          	user.setId(rs.getLong("Id"));
+	          	user.setName(rs.getString("UseName"));
+	          	user.setEmail(rs.getString("Mail"));
+	          	user.setPassword(rs.getString("Password"));
+	          	user.setTel(rs.getString("Tel"));
+	           
+	     }
+	     return user;
+	}
+	     public boolean updateUser(Customers u) throws SQLException {
+	        
+	        Connection connection = ConnectToDatabase.getConnecttion();
+	       // String sql = "UPDATE product SET product_id = ?, category_id = ?, product_name = ?, product_image = ?, product_image_forward = ?, product_image_back = ?, product_price = ?, product_description = ? WHERE product_id = ?";
+	        String sql = "UPDATE Custumers SET Id=?, Name=?, Email=?, Password=?,  Tel=? WHERE Id = ?";
+	        
+	        try {
+	            PreparedStatement ps = connection.prepareCall(sql);
+	         	ps.setLong(1, u.getId());
+	            ps.setString(2, u.getName());
+	            ps.setString(3, u.getEmail());
+	            ps.setString(4, u.getPassword());
+	            ps.setString(5, u.getTel());
+	            return ps.executeUpdate() == 1;
+	        } catch (SQLException ex) {
+	            Logger.getLogger(UserGet.class.getName()).log(Level.SEVERE, null, ex);
+	        }
+	        return false;
+	    }
 }
--- src/get/UserGet.java
+++ src/get/UserGet.java
@@ -4,127 +4,45 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import model.User;
 
 public class UserGet {
-	 public ArrayList<User> getListUser() throws SQLException {
+	 public boolean insertUser(User u) throws SQLException{
 	        Connection connection = ConnectToDatabase.getConnecttion();
-	        String sql = "SELECT * FROM uses";
-	        PreparedStatement ps = connection.prepareCall(sql);
-	        ResultSet rs = ps.executeQuery();
-	        ArrayList<User> list = new ArrayList<>();
-	        while (rs.next()) {
-	            User user = new User();
-	           user.setIduse(rs.getLong("IdUse"));
-	           user.setUsename(rs.getString("UseName"));
-	           user.setMail(rs.getString("Mail"));
-	           user.setPassword(rs.getString("Password"));
-	           user.setSdt(rs.getString("SDT"));
-	           user.setGioitinh(rs.getNString("Gioi_tinh"));
-	           user.setDia_chi(rs.getString("Dia_Chi"));
-	           user.setStatus(rs.getString("Status"));
-	           user.setName(rs.getString("Name"));
-	           list.add(user);
-	        }
-	        return list;
-	    }
-	   
-	    
-	    public boolean checkEmail(String name) throws SQLException{
-	    Connection connection = ConnectToDatabase.getConnecttion();
-	    String sql = "SELECT * FROM uses WHERE UseName = '" + name + "'";
-	    PreparedStatement ps;
-	    try {
-	        ps = connection.prepareCall(sql);
-	        ResultSet rs = ps.executeQuery();
-	        while(rs.next()){
-	            connection.close();
-	            return true;
-	        }
-	    } catch (SQLException ex) {
-	        Logger.getLogger(UserGet.class.getName()).log(Level.SEVERE,null, ex);
-	    }
-	    return false;
-	    }
-	    public boolean insertUser(User u) throws SQLException {
-	        Connection connection = ConnectToDatabase.getConnecttion();
-	        String sql = "INSERT INTO uses VALUES(?,?,?,?,?)";
-	        try {
+	        String sql = "INSERT INTO users VALUES(?,?,?,?)";
+	        try{
 	            PreparedStatement ps = connection.prepareCall(sql);
 	            ps.setLong(1, u.getIduse());
-	            ps.setString(2, u.getUsename());
-	            ps.setString(3, u.getMail());
-	            ps.setString(4, u.getPassword());
-	            ps.setString(5, u.getSdt());
+	            ps.setString(2, u.getMail());
+	            ps.setString(3, u.getPassword());
 	            ps.executeUpdate();
 	            return true;
-	        } catch (SQLException ex) {
-	            Logger.getLogger(UserGet.class.getName()).log(Level.SEVERE, null, ex);
+	        }catch(SQLException ex){
+	            Logger.getLogger(UserGet.class.getName()).log(Level.SEVERE,null,ex);
 	        }
 	        return false;
-	    }
-	   
-	     public User login(String name, String password) throws SQLException {
+	    } 
+	    
+	     public User login(String email, String password) throws SQLException {
 	        Connection con = ConnectToDatabase.getConnecttion();
-	        String sql = "select * from uses where UseName='" + name + "' and Password='" + password + "'";
+	        String sql = "select * from Uses where Name='" + email + "' and Password='" + password + "'";
 	        PreparedStatement ps;
 	        try {
 	            ps = (PreparedStatement) con.prepareStatement(sql);
 	            ResultSet rs = ps.executeQuery();
 	            if (rs.next()) {
-	                User u = new User();
-	                u.setIduse(rs.getLong("IdUse"));
-	 	           	u.setUsename(rs.getString("UseName"));
-	                u.setPassword(rs.getString("Password"));
+	                User ad = new User();
+	                ad.setIduse(rs.getLong("IdUse"));
+	                ad.setMail(rs.getString("Mail"));
+	                ad.setPassword(rs.getString("Password"));
 	                con.close();
-	                return u;
+	                return ad;
 	            }
 	        } catch (SQLException e) {
-	            e.printStackTrace();
 	        }
 	        return null;
-	    }
-	     
-	     public User getUser(long userID) throws SQLException {
-	     Connection connection = ConnectToDatabase.getConnecttion();
-	     String sql = "SELECT * FROM uses WHERE IdUse = '" + userID + "'";
-	     PreparedStatement ps = connection.prepareCall(sql);
-	     ResultSet rs = ps.executeQuery();
-	     User user = new User();
-	     while (rs.next()) {
-	         
-	          
-	           user.setIduse(rs.getLong("IdUse"));
-	           user.setUsename(rs.getString("UseName"));
-	           user.setMail(rs.getString("Mail"));
-	           user.setPassword(rs.getString("Password"));
-	           user.setSdt(rs.getString("SDT"));
-	           
-	     }
-	     return user;
-	}
-	     public boolean updateUser(User u) throws SQLException {
-	        
-	        Connection connection = ConnectToDatabase.getConnecttion();
-	       // String sql = "UPDATE product SET product_id = ?, category_id = ?, product_name = ?, product_image = ?, product_image_forward = ?, product_image_back = ?, product_price = ?, product_description = ? WHERE product_id = ?";
-	        String sql = "UPDATE uses SET IdUse=?, UseName=?, Mail=?, Password=?,  SDT=? WHERE IdUse = ?";
-	        
-	        try {
-	            PreparedStatement ps = connection.prepareCall(sql);
-	         	ps.setLong(1, u.getIduse());
-	            ps.setString(2, u.getUsename());
-	            ps.setString(3, u.getMail());
-	            ps.setString(4, u.getPassword());
-	            ps.setString(5, u.getSdt());
-	            ps.setLong(6, u.getIduse());
-	            return ps.executeUpdate() == 1;
-	        } catch (SQLException ex) {
-	            Logger.getLogger(UserGet.class.getName()).log(Level.SEVERE, null, ex);
-	        }
-	        return false;
 	    }
 
 }
